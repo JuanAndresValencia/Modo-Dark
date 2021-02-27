fe99f44e5662ae2c4e99a9b9b7310973231bb5aa
Juan Andres Valencia Zuluaga <juanvalencia2003dev@gmail.com>
2021-02-27 16:46:07 -0500

    Cambio de imagenes al alternar entre el light y dark mode

--- index.js
+++ index.js
@@ -7,17 +7,29 @@ document.addEventListener("DOMContentLoaded", () => {
        let documento = document.querySelector(".contenedor-total")
        documento.classList.toggle("dark")
        
+       
        if (documento.classList.contains("dark")){
            localStorage.setItem("darkmode", "true")
            let button = document.getElementById("button")
            button.style.paddingLeft = "37px"
+           let moonimg = document.getElementById("blackmode")
+           moonimg.style.display = "block"
+           let sunimg = document.getElementById("sunmode")
+           sunimg.style.display = "none"
        } else {
            localStorage.setItem("darkmode", "false")
            let button = document.getElementById("button")
            button.style.paddingLeft = "0px"
+           let sunimg = document.getElementById("sunmode")
+           sunimg.style.display = "block"
+           let moonimg = document.getElementById("blackmode")
+           moonimg.style.display = "none"
+
        }
        })
    
+       let moonimg = document.getElementById("blackmode")
+       moonimg.style.display = "none"
    
    
 
@@ -26,18 +38,22 @@ document.addEventListener("DOMContentLoaded", () => {
        documento.classList.add("dark")
        let button = document.getElementById("button")
        button.style.paddingLeft = "37px"
+       let moonimg = document.getElementById("blackmode")
+       moonimg.style.display = "block"
+       let sunimg = document.getElementById("sunmode")
+       sunimg.style.display = "none"
        
     } else{
        let documento = document.querySelector(".contenedor-total")
        documento.body.classList.remove("dark")
        let button = document.getElementById("button")
        button.style.paddingLeft = "0px"
+       let sunimg = document.getElementById("sunmode")
+       sunimg.style.display = "block"
+       let moonimg = document.getElementById("blackmode")
+       moonimg.style.display = "none"
        
-            
-
-       
-       
-   }
+    }
 
    
 
